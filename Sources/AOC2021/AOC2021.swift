af4ee5cf7427de06b7097108d76d5bee134ca2c9
Todd Olsen <todd.olsen@me.com>
2021-12-07 11:04:56 -0500

    Initial Commit

---
+++ Sources/AOC2021/AOC2021.swift
@@ -0,0 +1,6 @@
+public struct AOC2021 {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/AOC2021Tests/AOC2021Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AOC2021
+
+final class AOC2021Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AOC2021().text, "Hello, World!")
+    }
+}
