5ffe7e32c39c1ce13082b0a660a873f43ad8cd59
Todd Olsen <todd.olsen@me.com>
2021-12-07 21:47:05 -0500

    Add 2015 Day 1

--- Sources/AOC2015/Days/Day1.swift
+++ Sources/AOC2015/Days/Day1.swift
@@ -2,11 +2,30 @@ import AdventOfCode
 
 extension Day {
     private static func p1(input: String) -> String {
-        "[no answer]"
+        var position = 0
+        for char in input {
+            switch char {
+            case "(": position += 1
+            case ")": position -= 1
+            default: break
+            }
+        }
+        return String(describing: position)
     }
 
     private static func p2(input: String) -> String {
-        "[no answer]"
+        var position = 0
+        for (index, char) in zip(1..., input) {
+            switch char {
+            case "(": position += 1
+            case ")": position -= 1
+            default: break
+            }
+            if position == -1 {
+                return String(describing: index)
+            }
+        }
+        fatalError()
     }
 
     static let d1 = Day(p1, p2)
