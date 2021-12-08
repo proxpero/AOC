95a6af0da0516edcdd08796c31e618b089a4929c
Todd Olsen <todd.olsen@me.com>
2021-12-08 12:26:41 -0500

    Add 2015 Day 2

--- Sources/AOC2015/Days/Day2.swift
+++ Sources/AOC2015/Days/Day2.swift
@@ -1,12 +1,66 @@
 import AdventOfCode
+import Parsing
 
 extension Day {
+
+    struct Prism {
+
+        let length: Int
+        let width: Int
+        let height: Int
+
+        init(
+            length: Int,
+            width: Int,
+            height: Int
+        ) {
+            self.length = length
+            self.width = width
+            self.height = height
+        }
+
+        var surfaceArea: Int {
+            2 * length * width + 2 * width * height + 2 * height * length
+        }
+
+        var areaOfSmallestSide: Int {
+            min(length * width, width * height, height * length)
+        }
+
+        var surfaceAreaAndSmallestSide: Int {
+            surfaceArea + areaOfSmallestSide
+        }
+
+        var perimeterOfSmallestFace: Int {
+            min(length + width, width + height, height + length) * 2
+        }
+
+        var volume: Int {
+            length * width * height
+        }
+    }
+
+    private static func parse(input: String) -> [Prism] {
+        let prism = Int.parser()
+            .skip("x")
+            .take(Int.parser())
+            .skip("x")
+            .take(Int.parser())
+            .map(Prism.init)
+        let metrics = Many(prism, separator: "\n")
+        return metrics.parse(input)!
+    }
+
     private static func p1(input: String) -> String {
-        "[no answer]"
+        let prisms = parse(input: input)
+        let result = prisms.reduce(into: 0) { $0 += $1.surfaceAreaAndSmallestSide }
+        return String(describing: result)
     }
 
     private static func p2(input: String) -> String {
-        "[no answer]"
+        let prisms = parse(input: input)
+        let result = prisms.reduce(into: 0) { $0 += $1.perimeterOfSmallestFace + $1.volume }
+        return String(describing: result)
     }
 
     static let d2 = Day(p1, p2)
