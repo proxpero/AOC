7eaa3de8048e100b5db6736e70d169dedd88070d
Todd Olsen <todd.olsen@me.com>
2021-12-07 20:15:38 -0500

    Setup 2019

---
+++ Sources/AOC2019/Days/Day24.swift
@@ -0,0 +1,13 @@
+import AdventOfCode
+
+extension Day {
+    private static func p1(input: String) -> String {
+        "[no answer]"
+    }
+
+    private static func p2(input: String) -> String {
+        "[no answer]"
+    }
+
+    static let d24 = Day(p1, p2)
+}
