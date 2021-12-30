c7d04ef7bf51690bdd05b246120ad669bf9dc697
Todd Olsen <todd.olsen@me.com>
2021-12-30 12:04:41 -0500

    [2015.12] Fix Double value bug and reformat.

--- Sources/AOC2015/Days/Day12.swift
+++ Sources/AOC2015/Days/Day12.swift
@@ -2,7 +2,6 @@ import AdventOfCode
 import Parsing
 
 extension Day {
-
     private static func p1(input: String) -> String {
         func parse(input: String) -> Int {
             var sum = 0
@@ -32,26 +31,28 @@ extension Day {
     private static func p2(input: String) -> String {
         let j = json.parse(input)!
         let result = j.numberValueExceptInRed()
-        return String(describing: result)
+        return String(describing: Int(result))
     }
 
     static let d12 = Day(p1, p2)
 }
 
 private enum JSON: Equatable {
-  indirect case array([JSON])
-  case boolean(Bool)
-  case null
-  case number(Double)
-  indirect case object([String: JSON])
-  case string(String)
+    indirect case array([JSON])
+    case boolean(Bool)
+    case null
+    case number(Double)
+    indirect case object([String: JSON])
+    case string(String)
 }
 
 extension JSON {
     func numberValueExceptInRed() -> Double {
         var result = 0.0
         switch self {
-        case .boolean, .null, .string:
+        case .boolean,
+             .null,
+             .string:
             break
         case .number(let value):
             result += value
@@ -79,118 +80,118 @@ extension JSON {
 // From the swift-parsing library (Sources/swift-parsing-benchmark/JSON.swift)
 
 private var json: AnyParser<Substring.UTF8View, JSON> {
-  Skip(Whitespace())
-    .take(
-      object
-        .orElse(array)
-        .orElse(string)
-        .orElse(number)
-        .orElse(boolean)
-        .orElse(null)
-    )
-    .skip(Whitespace())
-    .eraseToAnyParser()
+    Skip(Whitespace())
+        .take(
+            object
+                .orElse(array)
+                .orElse(string)
+                .orElse(number)
+                .orElse(boolean)
+                .orElse(null)
+        )
+        .skip(Whitespace())
+        .eraseToAnyParser()
 }
 
 // MARK: Object
 
 private let object = "{".utf8
-  .take(
-    Many(
-      Skip(Whitespace())
-        .take(stringLiteral)
-        .skip(Whitespace())
-        .skip(":".utf8)
-        .take(Lazy { json }),
-      into: [:],
-      separator: ",".utf8.skip(Whitespace())
-    ) { object, pair in
-      let (name, value) = pair
-      object[name] = value
-    }
-  )
-  .skip("}".utf8)
-  .map(JSON.object)
+    .take(
+        Many(
+            Skip(Whitespace())
+                .take(stringLiteral)
+                .skip(Whitespace())
+                .skip(":".utf8)
+                .take(Lazy { json }),
+            into: [:],
+            separator: ",".utf8.skip(Whitespace())
+        ) { object, pair in
+            let (name, value) = pair
+            object[name] = value
+        }
+    )
+    .skip("}".utf8)
+    .map(JSON.object)
 
 // MARK: Array
 
 private let array = "[".utf8
-  .take(
-    Many(
-      Lazy { json },
-      separator: ",".utf8
+    .take(
+        Many(
+            Lazy { json },
+            separator: ",".utf8
+        )
     )
-  )
-  .skip("]".utf8)
-  .map(JSON.array)
+    .skip("]".utf8)
+    .map(JSON.array)
 
 // MARK: String
 
 private let unicode = Prefix(4) {
-  (.init(ascii: "0") ... .init(ascii: "9")).contains($0)
-    || (.init(ascii: "A") ... .init(ascii: "F")).contains($0)
-    || (.init(ascii: "a") ... .init(ascii: "f")).contains($0)
+    (.init(ascii: "0") ... .init(ascii: "9")).contains($0)
+        || (.init(ascii: "A") ... .init(ascii: "F")).contains($0)
+        || (.init(ascii: "a") ... .init(ascii: "f")).contains($0)
 }
 .compactMap {
-  UInt32(Substring($0), radix: 16)
-    .flatMap(UnicodeScalar.init)
-    .map(Character.init)
+    UInt32(Substring($0), radix: 16)
+        .flatMap(UnicodeScalar.init)
+        .map(Character.init)
 }
 
 private let escape = "\\".utf8
-  .take(
-    "\"".utf8.map { "\"" }
-      .orElse("\\".utf8.map { "\\" })
-      .orElse("/".utf8.map { "/" })
-      .orElse("b".utf8.map { "\u{8}" })
-      .orElse("f".utf8.map { "\u{c}" })
-      .orElse("n".utf8.map { "\n" })
-      .orElse("r".utf8.map { "\r" })
-      .orElse("t".utf8.map { "\t" })
-      .orElse(unicode)
-  )
+    .take(
+        "\"".utf8.map { "\"" }
+            .orElse("\\".utf8.map { "\\" })
+            .orElse("/".utf8.map { "/" })
+            .orElse("b".utf8.map { "\u{8}" })
+            .orElse("f".utf8.map { "\u{c}" })
+            .orElse("n".utf8.map { "\n" })
+            .orElse("r".utf8.map { "\r" })
+            .orElse("t".utf8.map { "\t" })
+            .orElse(unicode)
+    )
 
 private let literal = Prefix(1...) {
-  $0 != .init(ascii: "\"") && $0 != .init(ascii: "\\")
+    $0 != .init(ascii: "\"") && $0 != .init(ascii: "\\")
 }
 .map { String(Substring($0)) }
 
 private enum StringFragment {
-  case escape(Character)
-  case literal(String)
+    case escape(Character)
+    case literal(String)
 }
 
 private let fragment = literal.map(StringFragment.literal)
-  .orElse(escape.map(StringFragment.escape))
+    .orElse(escape.map(StringFragment.escape))
 
 private let stringLiteral = "\"".utf8
-  .take(
-    Many(fragment, into: "") {
-      switch $1 {
-      case let .escape(character):
-        $0.append(character)
-      case let .literal(string):
-        $0.append(contentsOf: string)
-      }
-    }
-  )
-  .skip("\"".utf8)
+    .take(
+        Many(fragment, into: "") {
+            switch $1 {
+            case .escape(let character):
+                $0.append(character)
+            case .literal(let string):
+                $0.append(contentsOf: string)
+            }
+        }
+    )
+    .skip("\"".utf8)
 
 private let string =
-  stringLiteral
-  .map(JSON.string)
+    stringLiteral
+        .map(JSON.string)
 
 // MARK: Number
 
 private let number = Double.parser(of: Substring.UTF8View.self)
-  .map(JSON.number)
+    .map(JSON.number)
 
 // MARK: Boolean
 
 private let boolean = Bool.parser(of: Substring.UTF8View.self)
-  .map(JSON.boolean)
+    .map(JSON.boolean)
 
 // MARK: Null
 
 private let null = "null".utf8
-  .map { JSON.null }
+    .map { JSON.null }
